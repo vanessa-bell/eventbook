ce002cd9d34ab4a231eba1861d0ce305c6dd8b22
Vanessa Bell <vanessa.bell14@gmail.com>
2016-07-28 12:14:12 -0700

    added concert-mapper

--- app/config/routes.py
+++ app/config/routes.py
@@ -5,4 +5,8 @@ routes['default_controller'] = 'Users'
 routes['/home'] = 'Users#home'
 routes['/event'] = 'Users#event'
 routes['/favorites'] = 'Users#favorites'
+routes['/concert-mapper'] = 'Users#concertmapper'
 routes['/profile'] = 'Users#profile'
+routes['POST']['/search-results'] = 'Users#search'
+routes['/add-favorite'] = 'Users#add'
+
--- app/controllers/Users.py
+++ app/controllers/Users.py
@@ -1,4 +1,5 @@
 from system.core.controller import *
+import json
 
 class Users(Controller):
     def __init__(self, action):
@@ -19,5 +20,48 @@ class Users(Controller):
     def favorites(self):
         return self.load_view('favorites.html')
 
+    def add(self):
+        print "hit add route"
+        #need to add event data to db here...basically jData[x]['id'], jData[x]['title'], jData[x]['artists'][0]['image_url'],{{jData[x]['formatted_datetime']}},jData[x]['ticket_url']
+        return redirect('/favorites')
+
     def profile(self):
         return self.load_view('profile.html')
+
+    def concertmapper(self):
+        return self.load_view('concert-mapper.html')
+
+    def search(self,methods="POST"):
+        artist= request.form['artist']
+        location = request.form['location']
+        radius = request.form['radius']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        print "successful form submission"
+        url2 = 'http://api.bandsintown.com/artists/' + artist + '/events/recommended?location=' + location + '&date=' +start_date+','+end_date+'&radius=' + radius + '&app_id=eventbook&api_version=2.0&format=json'
+        r = requests.get(url2)
+        
+        jData = json.loads(r.content)
+        length = len(jData)
+        
+        
+        if not jData:
+            flash('Sorry, there are no results for that artist with your criteria. Please try again!','error')
+            return redirect('/concert-mapper')
+        else:       
+            results = []
+            for i in range(0,length):
+                obj = []
+                title = jData[i]['title']
+                lat = float(jData[i]['venue']['latitude'])
+                lng = float(jData[i]['venue']['longitude'])
+                obj.append(title)
+                obj.append(lat)
+                obj.append(lng)
+                results.append(obj)
+               
+            print results
+        return self.load_view('success.html',jData=jData, lat=lat,lng=lng, artist=artist, length=length,results=results)
+
+
+
